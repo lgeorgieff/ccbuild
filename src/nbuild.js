af95970578eb500d4b0e7aad0bcba9f4a103ed13
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-01-23 23:32:39 +0100

    cli: Add support --config-help

--- src/nbuild.js
+++ src/nbuild.js
@@ -55,6 +55,54 @@ function getUsage () {
 }
 
 /**
+ * Get a help text for the configuration file.
+ *
+ * @private
+ *
+ * @returns {Promise} A promise that holds the help text for the config file format as a string value.
+ */
+function getConfigFileHelp () {
+    var deferred =  Q.defer();
+    utils.getSelfName().then(function (selfName) {
+        deferred.resolve(
+            'The configuration files for ' + selfName + ' use the JSON format and are of the\n' +
+                'following form:\n\n' +
+                '{\n' +
+                '  "sources": [<source file paths to be included in all compilation units defined in this config>],\n' +
+                '  "externs": [<extern file paths to be included in all compilation units defined in this config>],\n' +
+                '  "buildOptions": [<options to be used for all compilation units defined in this config>],\n' +
+                '  "compilationUnits": {\n' +
+                '    "unit 1": {\n' +
+                '      "externs": [<source file paths to be used only for this compilation unit>],\n' +
+                '      "sources": [<extern file paths to be used only for this compilation unit>],\n' +
+                '      "buildOptions": [<options to be used only for this compilation unit>]\n' +
+                '    },\n' +
+                '    "unit 2": {\n' +
+                '      "externs": [<source file paths to be used only for this compilation unit>],\n' +
+                '      "sources": [<extern file paths to be used only for this compilation unit>],\n' +
+                '      "buildOptions": [<options to be used only for this compilation unit>]\n' +
+                '    },\n' +
+                '  }\n' +
+                '  "next": {\n' +
+                '    "file path to the next config relative to this config": {\n' +
+                '      "inheritSources": <boolean>,\n' +
+                '      "inheritExterns": <boolean>,\n' +
+                '      "inheritBuildOptions": <boolean>\n' +
+                '    }\n' +
+                '    "file path to another config relative to this config": {\n' +
+                '      "inheritSources": <boolean>,\n' +
+                '      "inheritExterns": <boolean>,\n' +
+                '      "inheritBuildOptions": <boolean>\n' +
+                '    }\n' +
+                '  }\n' +
+                '}\n\n' +
+                'Note: buildOptions can be either an array of strings or an object as specified\n' +
+                'at https://www.npmjs.com/package/google-closure-compiler#specifying-options.');
+    }).catch(deferred.reject);
+    return deferred.promise;
+}
+
+/**
  * Parse all CLI arguments and return an object with the corresponding properties.
  * In case of error, the script exits with exit code 2. Some properties, e.g. --version lead to an exit with exit code 0.
  *
@@ -93,6 +141,13 @@ function parseCliArgs (args) {
         case '-c':
             break;
         case '--config-help':
+            getConfigFileHelp().then(function (configHelp) {
+                console.log(configHelp);
+                process.exit(0);
+            }).catch(function (err) {
+                deferred.reject(err);
+            });
+            i = args.length;
             break;
         case '--closure-help':
             break;
