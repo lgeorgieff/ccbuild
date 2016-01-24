8a3b3d4c899ba168191b84effd5c605ad3804edf
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-01-24 14:39:04 +0100

    lint: Add extern for google-closure-compiler

---
+++ tools/closure_compiler_externs/cc.js
@@ -0,0 +1,21 @@
+var CC = {};
+
+CC.compiler = {};
+
+/**
+ * @type {string}
+ * @const
+ */
+CC.compiler.COMPILER_PATH;
+
+/**
+ * @type {string}
+ * @const
+ */
+CC.compiler.CONTRIB_PATH;
+
+/**
+ * @type {string}
+ * @const
+ */
+CC.compiler.jar_path;
