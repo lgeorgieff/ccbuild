ffae0f32d88e73e407d3d662f1c98169c7a0ca80
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-02-13 00:16:05 +0100

    nbuild: Add unit tests for default and non-default configs

--- test/unit_test/nbuild.spec.js
+++ test/unit_test/nbuild.spec.js
@@ -196,7 +196,71 @@ describe('nbuild', function () {
         this.resourcesToDelete.push(configPath3);
     });
 
-    xit('compile with config and default config', function () {});
+    it('compile with config and default config', function (done) {
+        var config1 = {
+            sources: ['./data/source1.js'],
+            externs: ['data/externs1.js'],
+            buildOptions: [
+                '--compilation_level', 'ADVANCED_OPTIMIZATIONS',
+                '--warning_level', 'VERBOSE',
+                '--env', 'CUSTOM',
+                '--flagfile', './data/test_flagfile'
+            ],
+            compilationUnits: {
+                unit1: {
+                }
+            }
+        };
+        var config2 = {
+            sources: config1.sources,
+            externs: config1.externs,
+            buildOptions: config1.buildOptions,
+            compilationUnits: {
+                unit2: {
+                    sources: ['data/source2.js'],
+                    externs: ['./data/externs2.js']
+                }
+            }
+        };
+        var config3 = {
+            sources: config1.sources,
+            externs: config1.externs,
+            buildOptions: config1.buildOptions,
+            compilationUnits: {
+                unit3: {
+                    sources: ['./data/source3.js', './data/source4.js'],
+                    externs: ['./data/externs2.js', 'data/externs3.js']
+                }
+            }
+        };
+        var configPath1 = path.join(__dirname, 'config1.nbuild');
+        fs.writeFileSync(configPath1, JSON.stringify(config1, null, 2));
+        var configPath2 = path.join(__dirname, 'config2');
+        fs.writeFileSync(configPath2, JSON.stringify(config2, null, 2));
+        var configPath3 = path.join(__dirname, 'config3');
+        fs.writeFileSync(configPath3, JSON.stringify(config3, null, 2));
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        child_process.exec('node ../../src/nbuild.js -c ' + configPath2 + ' --config ' + configPath3, {cwd: __dirname},
+                           function (err, stdout, stderr) {
+                               if (err) {
+                                   done.fail(err);
+                               } else {
+                                   expect(stdout.length).toBeGreaterThan(0);
+                                   expect(stdout.indexOf('=== unit1 =================================================' +
+                                                         '====================\n')).toBe(-1);
+                                   expect(stdout.indexOf('=== unit2 =================================================' +
+                                                         '====================\n')).not.toBe(-1);
+                                   expect(stdout.indexOf('=== unit3 =================================================' +
+                                                         '====================\n')).not.toBe(-1);
+                                   expect(stderr.length).toBeGreaterThan(0);
+                                   done();
+                               }
+                           });
+        // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+        this.resourcesToDelete.push(configPath1);
+        this.resourcesToDelete.push(configPath2);
+        this.resourcesToDelete.push(configPath3);
+    });
 
     xit('compile with config hierarchy', function () {
         // use relative paths
