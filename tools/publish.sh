21844041f546b3b6896e2ec0a431c6de989cffd9
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-02-14 10:16:34 +0100

    infra: Add publish script

---
+++ tools/publish.sh
@@ -0,0 +1,9 @@
+### This script uses the correct package.json file and publishes the entire package.
+
+rm package.json
+ln -s package.json.publish package.json
+
+npm publish
+
+rm package.json
+ln -s package.json.dev package.json
