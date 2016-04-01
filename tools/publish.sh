e4ed73882b7f34d0d98b55507fb34476ad7ca8bd
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-04-01 21:52:06 +0200

    tools: Add clean up operations to trap for ctrl-c

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,9 +1,16 @@
 ### This script uses the correct package.json file and publishes the entire package.
 
+clean_up () {
+    rm package.json
+    ln -s package.json.dev package.json
+}
+
+trap clean_up 2
+
+npm run generate_doc
+
 rm package.json
 cp package.json.publish package.json
 
 npm publish
-
-rm package.json
-ln -s package.json.dev package.json
+clean_up
