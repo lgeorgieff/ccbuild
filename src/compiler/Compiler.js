a7dcf2ae1b101591633c8b1ca2305ffe2400d37d
Lukas Georgieff <lukas@brunhild.fritz.box>
2016-12-03 02:37:17 +0100

    src: Add abstract base class for compilers

---
+++ src/compiler/Compiler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * The constructor function for {@link Compiler}.
+ *
+ * @classdesc This class is an abstract base class for a compiler that returns the final JavaScript code.
+ *
+ * @constructor
+ *
+ * @throws {Error} Thrown if this class gets instaniated.
+ */
+function Compiler () { }
+
+/**
+ * Invokes the Closure Compiler and returns a Promise holding the compilation results.
+ *
+ * @abstract
+ *
+ * @returns {QPromise<Object>} A promise holding the compilation result.
+ * @param {CompilerConfiguration} compilationUnit The compiler configuration for the current compilation unit.
+ *
+ * @throws {Error} Thrown if compilationUnit is of a wrong type.
+ */
+Compiler.prototype.compile = function (compilationUnit) { };
+
+module.exports = Compiler;
