eca0348f8db77c669f67ee350162576e149978ba
Lukas Georgieff <lukas.georgieff@hotmail.com>
2016-12-16 03:33:57 +0100

    src: Implement the class CCCache

--- src/compiler/CCCache.js
+++ src/compiler/CCCache.js
@@ -4,34 +4,140 @@
  * @ignore
  * @suppress {duplicate}
  */
+var crypto = require('crypto');
+
+/**
+ * @ignore
+ * @suppress {duplicate}
+ */
+var stream = require('stream');
+
+/**
+ * @ignore
+ * @suppress {duplicate}
+ */
+var fs = require('fs');
+
+/**
+ * @ignore
+ * @suppress {duplicate}
+ */
+var path = require('path');
+
+/**
+ * @ignore
+ * @suppress {duplicate}
+ */
+var Q = require('Q');
+
+/**
+ * @ignore
+ * @suppress {duplicate}
+ */
 var NotFoundInCacheError = /** @type {function (new:NotFoundInCacheError, string, string, string): undefined}*/
     (require('./NotFoundInCacheError.js'));
 
 /**
- * The constructor function for {@link CCCache}.
+ * Contains for each cache folder location an existing {@link CCCache} instance.
+ *
+ * @private
+ */
+var instances = {};
+
+/**
+ * The file name of the bibliography file that contians the hash index for the compilation cache.
+ *
+ * @private
+ * @const
+ */
+var BIB_FILE_NAME = 'bibliography.json';
+
+/**
+ * The constructor function for {@link CCCache}. This class is a singleton per cache folder to avoid concurrency on
+ * file system level.
  *
  * @classdesc This class is responsible for caching compilation units.
  *
  * @constructor
  *
  * @param {!string} cacheFolder A folder path where CCBuild stores all cached content.
- * @throws {Error} Thrown if cacheFolder is not a string or empty.
+ * @throws {Error} Thrown if cacheFolder is an empty string.
  */
-function CCCache (cacheFolder) { }
+function CCCache (cacheFolder) {
+    if (instances[cacheFolder]) {
+        return instances[cacheFolder];
+    } else {
+        instances[cacheFolder] = this;
+    }
+
+    if (cacheFolder.length === 0) {
+        throw new Error('"cacheFolder" must not be an empty string');
+    }
+    this._cacheFolder = cacheFolder;
+    this._bibliography = {};
+    this._readBibliographyPromise = null;
+}
 
 /**
- * Tries to get a cached compilation result. In case there is nothing cached yet for the given compilation unit or the
- * data held in the cache is outdated, an error is returned.
+ * Read the index file on FS level and load the caech index.
+ *
+ * @private
+ *
+ * @returns {QPromise<Object>} A promise holding the JSON data of the loaded index file or an error in case of FS or
+ *          parsing problems.
+ */
+CCCache.prototype._readBibliography = function () {
+    if (this._readBibliographyPromise) {
+        return this._readBibliographyPromise;
+    }
+
+    var self = this;
+    var deferred = Q.defer();
+    fs.readFile(path.join(this._cacheFolder, BIB_FILE_NAME), 'utf8', function (err, data) {
+        if (err && err.code !== 'ENOENT') {
+            // The operation was only considered as failed when the file could not be read but did exist.
+            // In it did not exist, it will be created during the next write operation.
+            deferred.reject(err);
+        } else {
+            try {
+                self._bibliography = JSON.parse(data);
+                deferred.resolve(self._bibliography);
+            } catch (parserError) {
+                deferred.reject('Could not read index file of cache "' + self._cacheFolder + '" due to ' + parserError);
+            }
+        }
+    });
+
+    this._readBibliographyPromise = deferred.promise;
+    return this._readBibliographyPromise;
+};
+
+/**
+ * Tries to get a cached compilation result. In case there is nothing cached yet for the given compilation unit an error
+ * is returned. In case the data held in the cache is outdated, the cache for this compilation unit is cleaned.
  *
  * @returns {QPromise<Object>} A promise holding the cached compilation result. In case the requested compilation unit
  *          is not cached the promise is rejected with a {@link NotFoundInCacheError}.
  * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the current compilation unit.
- *
- * @throws {Error} Thrown if compilationUnit is of a wrong type.
- * @throws {Error} Thrown if the access to the cache failed.
  */
 CCCache.prototype.get = function (compilationUnit) {
-    return null;
+    var self = this;
+    return this._readBibliography()
+        .then(function () {
+            var compilationUnitHash = self._generateHash(compilationUnit);
+            var deferred = Q.defer();
+            if (self._bibliography[compilationUnit.unitName] === compilationUnitHash) {
+                return self._getCachedResult(compilationUnit.unitName, compilationUnitHash);
+            } else if (self._bibliography[compilationUnit.unitName] !== undefined) {
+                return self.clean(compilationUnit)
+                    .then(function () {
+                        return null;
+                    });
+            } else {
+                deferred.reject(NotFoundInCacheError(compilationUnit.unitName, compilationUnitHash, self._cacheFolder));
+            }
+            return deferred.promise;
+        });
 };
 
 /**
@@ -42,29 +148,178 @@ CCCache.prototype.get = function (compilationUnit) {
  *          is not cached the promise is rejected with a {@link NotFoundInCacheError}.
  * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the requested compilation unit.
  * @param {Object} compilationResult The result of the Google Closure Compiler that will be cached.
- *
- * @throws {Error} Thrown if compilationUnit is of a wrong type.
- * @throws {Error} Thrown if the access to the cache failed.
  */
 CCCache.prototype.write = function (compilationUnit, compilationResult) {
-    return null;
+    var self = this;
+    return this._readBibliography()
+        .then(function () {
+            var compilationUnitHash = self._bibliography[compilationUnit.unitName];
+            if (compilationUnitHash) {
+                return self.clean(compilationUnit);
+            }
+        })
+        .then(function () {
+            return self._generateHash(compilationUnit);
+        })
+        .then(function (compilationUnitHash) {
+            self._bibliography[compilationUnit.unitName] = compilationUnitHash;
+            var deferred = Q.defer();
+            fs.writeFile(path.join(self._cacheFolder, compilationUnitHash + 'json'), 'utf8', function (err) {
+                if (err) {
+                    deferred.reject('Could not write cache for the compilation unit "' + compilationUnit.unitName +
+                                    '" due to ' + err);
+                } else {
+                    deferred.resolve();
+                }
+            });
+            return deferred.promise;
+        });
 };
 
 /**
- * Write the generated JavaScript code into the cache. In case the compilaton unit is already cached it will be
- * overwritten.
+ * This class must be called to persist the cache before the programme will exit.
+ *
+ * @return {QPromise} A promise that is resolved in case the cache is completetly persisted or a rejeted in case of an
+ *         error.
+ */
+CCCache.prototype.destroy = function () {
+    var self = this;
+    return (this._readBibliographyPromise || Q.resolve())
+        .then(function () {
+            var deferred = Q.defer();
+            try {
+                var content = JSON.stringify(self._bibliography);
+                fs.writeFile(path.join(self._cacheFolder, BIB_FILE_NAME), content, 'utf8', function (err) {
+                    if (err) {
+                        deferred.reject('Could not persist index file for cache due to' + err);
+                    } else {
+                        deferred.resolve();
+                    }
+                });
+            } catch (err) {
+                deferred.reject('Could not persist inde file for cache due to' + err);
+            }
+
+            return deferred.promise;
+        });
+};
+
+/**
+ * Remove the entry from the bibliography file and remove the actual cached compilation result.
+ *
+ * @returns {QPromise<Object>} A promise that is resolved in case of success or rejected otherwise.
+ * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the requested compilation unit.
+ */
+CCCache.prototype.clean = function (compilationUnit) {
+    return this._readBibliography()
+        .then(function () {
+            var compilationUnitHash = this._bibliography[compilationUnit.unitName];
+            if (compilationUnitHash) {
+                var deferred = Q.defer();
+                fs.unlink(path.join(self._cacheFolder, compilationUnitHash + '.json'), function (err) {
+                    if (err) {
+                        deferred.reject('Failed to clean the cache for the compilation unit "' +
+                                        compilationUnit.unitName + '" due to ' + err);
+                    } else {
+                        deferred.resolve(true);
+                    }
+                });
+                return deferred.promise;
+            } else {
+                return false;
+            }
+        })
+        .then(function (entryFound) {
+            delete this._bibliography[compilationUnit.unitName];
+        });
+};
+
+/**
+ * Create a {@link Readable} stream of the given string.
+ *
+ * @private
+ * @returns {Readable} A stream for the given stirng value
+ * @param {string} item A string value.
+ */
+CCCache.prototype._stringToStream = function (item) {
+    if (item instanceof stream.ReadableStream) {
+        return item;
+    } else if (typeof (item) === 'string') {
+        var s = new stream.Readable();
+        s.push(item);
+        s.push(null);
+        return s;
+    } else {
+        throw new Error('Error occurred when handling cache at "' + this._cacheFolder + '" "item" must be either a ' +
+                        'string or a ReadableStream');
+    }
+};
+
+/**
+ * Generate a hash for all given items.
  *
  * @private
  *
- * @returns {QPromise<Object>} A promise holding the cached compilation result. In case the requested compilation unit
- *          is not cached the promise is rejected with a {@link NotFoundInCacheError}.
- * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the requested compilation unit.
- *
- * @throws {Error} Thrown if compilationUnit is of a wrong type.
- * @throws {Error} Thrown if the access to the cache failed.
- * @throws {NotFoundInCacheError} Thrown if no result is found in the cache for the requested compilation unit.
+ * @returns {QPromise<string>} A promise either holding a string in hex representation of a sha-256 hash or holding an
+ *          {@link Error} in case of rejection.
+ * @param {Array<Readable>} items A list of {@link Readable} streams that are piped into the hashing function and will
+ *        result in the final sha256-hash.
  */
-CCCache.prototype._cleanCache = function (compilationUnit) {
+CCCache.prototype._generateHashForItems = function (items) {
+    var hash = crypto.createHash('sha256');
+    (items || []).forEach(function (dataItem) {
+        dataItem.pipe(hash);
+    });
+    hash.setEncodding('hex');
+
+    var deferred = Q.defer();
+    return new Promise(function (resolve, reject) {
+        var result = '';
+        hash.on('data', function (data) {
+            result += data;
+        });
+        hash.on('end', function () {
+            deferred.resolve(result);
+        });
+        hash.on('error', function (err) {
+            deferred.reject(err);
+        });
+    });
+    return deferred.promise;
+};
+
+/**
+ * Create a file stream (Readable) for each given file path.
+ *
+ * @private
+ *
+ * @returns {Array<Readable>} A list of {@link Readable} streams for all given file paths.
+ * @param {Array<string>} filePaths Paths to files that are returned as {@link Readable} streams.
+ */
+CCCache.prototype._getFileStreams = function (filePaths) {
+    return (filePaths || []).map(function (filePath) {
+        return fs.createReadStream(filePath, {encoding: 'utf8'});
+    });
+};
+
+/**
+ * Get a file stream (ReadStream) that represents the flagfile which was passed via the options argument.
+ *
+ * @private
+ *
+ * @returns {?Readable} The stream for a flagfile specified via `options`. In case no flagfile is specified in `options`
+ *          `null` is returned.
+ * @param {!Array<string>} options The build options for the closure compiler.
+ */
+CCCache.prototype._getFlagfileStream = function (options) {
+    for (var i = 0; i !== options.length; ++i) {
+        if (options[i] === '--flagfile') {
+            break;
+        }
+    }
+    if (i < options.length - 1) {
+        return this._getFileStreams([options[i + 1]]);
+    }
     return null;
 };
 
@@ -75,42 +330,74 @@ CCCache.prototype._cleanCache = function (compilationUnit) {
  *
  * @returns {QPromise<string>} A promise holding the hash that represents the passed compilation unit.
  * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the requested compilation unit.
- *
- * @throws {Error} Thrown if compilationUnit is of a wrong type.
- * @throws {Error} Thrown if file access failed.
  */
 CCCache.prototype._generateHash = function (compilationUnit) {
-    // A compilation unit is hashed by the following process
-    // - unitName: is ignored
-    // - globalSources: file contents are hashed
-    // - unitSources: file contents are hashed
-    // - globalExterns: file contents are hashed
-    //   - flagfile: file content is hashed
-    // - unitExterns: file contents are hashed
-    //   - flagfile: file content is hashed
-    // - globalBuildOptions: values are hashed
-    // - unitBuildOptions: values are hashed
-    // - outputFile: value is hashed
-    // - unitWarningsFilterFile: file content are hashed
-    // - globalWarningsFilterFile: file content are hashed
-    return null;
+    var self = this;
+    var allFiles = compilationUnit.globalSources
+        .concat(compilationUnit.unitSources)
+        .concat(compilationUnit.globalExterns)
+        .concat(compilationUnit.unitExterns)
+        .concat(compilationUnit.globalWarningsFilterFile)
+        .concat(compilationUnit.unitWarningsFilterFile);
+    var globalFlagfile = this._getFlagfileStream(compilationUnit.globalBuildOptions);
+    if (globalFlagfile) {
+        allFiles.push(globalFlagfile);
+    }
+    var unitFlagfile = this._getFlagfileStream(compilationUnit.unitBuildOptions);
+    if (unitFlagfile) {
+        allFiles.push(unitFlagfile);
+    }
+    var itemsToBeHashed = this._getFileStreams(allFiles)
+        .concat(compilationUnit.globalBuildOptions).map(function (option) {
+            return self._stringToStream(option);
+        })
+        .concat(compilationUnit.unitBuildOptions).map(function (option) {
+            return self._stringToStream(option);
+        });
+
+    return this._generateHashForItems(itemsToBeHashed)
+        .then(function (hashValue) {
+            return hashValue;
+        })
+        .finally(function () {
+            itemsToBeHashed.forEach(function (itemToBeHashed) {
+                itemToBeHashed.close();
+            });
+        });
 };
 
 /**
- * Get the result for a compilation unit form the cached data.
+ * Get the result for a compilation unit from the cached data.
  *
  * @private
  *
  * @returns {QPromise<Object>} A promise holding the cached compilation result. In case the requested compilation unit
  *          is not cached the promise is rejected with a {@link NotFoundInCacheError}.
- * @param {!CompilerConfiguration} compilationUnit The compiler configuration for the requested compilation unit.
+ * @param {string} compilationUnitName The name of the compilation unit that is searched in the cache.
+ * @param {string} compilationUnitHash The calculated has of the compilation unit that is searched in the cache.
  *
- * @throws {Error} Thrown if compilationUnit is of a wrong type.
- * @throws {Error} Thrown if the access to the cache failed.
+ * @throws {Error} Thrown if the access or processing to the cache failed.
  * @throws {NotFoundInCacheError} Thrown if no result is found in the cache for the requested compilation unit.
  */
-CCCache.prototype._getCachedResult = function (compilationUnit) {
-    return null;
+CCCache.prototype._getCachedResult = function (compilationUnitName, compilationUnitHash) {
+    var cachePath = path.join(this._cacheFolder, compilationUnitHash + '.json');
+
+    var deferred = Q.defer();
+    fs.readFile(cachePath, 'utf8', function (err, data) {
+        if (err && err.code === 'ENOENT') {
+            deferred.reject(new NotFoundInCacheError(compilationUnitName, compilationUnitHash, this._cacheFolder));
+        } else if (err) {
+            deferred.reject(new Error(err));
+        } else {
+            try {
+                deferred.resolve(JSON.parse(data));
+            } catch (err) {
+                deferred.reject(new Error('Could not parse the cached file ' + cachePath + ': ' + err));
+            }
+        }
+    });
+
+    return deferred.promise;
 };
 
 module.exports = CCCache;
